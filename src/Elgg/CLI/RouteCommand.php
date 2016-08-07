54f33e99554f9a5123d6a2dfdf65b2875581389f
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-08-07 14:55:41 +0200

    chore(commands): can now specify HTTP method for route command

--- src/Elgg/CLI/RouteCommand.php
+++ src/Elgg/CLI/RouteCommand.php
@@ -20,6 +20,7 @@ class RouteCommand extends Command {
 				->setDescription('Route a request for a given path')
 				->addArgument('uri', InputArgument::REQUIRED, 'URI of the request (route path)')
 				->addArgument('ajax', InputArgument::OPTIONAL, 'AJAX api version (0 for non-ajax)', 0)
+				->addArgument('method', InputArgument::OPTIONAL, 'HTTP method', 'GET')
 				->addOption('tokens', null, InputOption::VALUE_NONE, 'Add CSRF tokens to the request');
 	}
 
@@ -30,19 +31,21 @@ class RouteCommand extends Command {
 
 		$uri = $this->argument('uri');
 		$ajax = (int) $this->argument('ajax');
+		$method = $this->argument('method');
 		$add_csrf_tokens = $this->option('tokens');
 
 		$site_url = elgg_get_site_url();
 		$uri = substr(elgg_normalize_url($uri), strlen($site_url));
 		$path_key = Application::GET_PATH_KEY;
 
+		$parameters = [];
 		if ($add_csrf_tokens) {
 			$ts = time();
 			$parameters['__elgg_ts'] = $ts;
 			$parameters['__elgg_token'] = _elgg_services()->actions->generateActionToken($ts);
 		}
 
-		$request = Request::create("?$path_key=" . urlencode($uri), 'GET');
+		$request = Request::create("?$path_key=" . urlencode($uri), $method, $parameters);
 
 		$cookie_name = _elgg_services()->config->getCookieConfig()['session']['name'];
 		$session_id = _elgg_services()->session->getId();
